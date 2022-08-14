864486bc5c3008d1a1dc5c51ef83d88ae72a9dc4
Alysson <aharon_curitiba@hotmail.com>
2022-08-14 14:45:57 -0300

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+const question = document.querySelector('#question');
+const answersBox = document.querySelector('#answers-box');
+const quizzContainer = document.querySelector('#quizz-container');
+const scoreContainer = document.querySelector('#score-container');
+const letters = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
 
 // Perguntas
 const questions = [
@@ -65,3 +72,134 @@ const questions = [
     ]
   },
 ]
+
+function init() {
+
+  createQuestion(0)
+
+}
+
+function createQuestion(i) {
+
+  const oldButtons = answersBox.querySelectorAll('button');
+
+  oldButtons.forEach(function(btn) {
+    btn.remove()
+  })
+
+  const questionText = question.querySelector('#question-text');
+  const questinNumber = question.querySelector('#question-number');
+
+  questionText.textContent = questions[i].question
+  questinNumber.textContent = i + 1
+
+  questions[i].answers.forEach(function(answer, i) {
+
+    const answerTemplate = document.querySelector('.answer-template').cloneNode(true);
+
+    const letterBtn = answerTemplate.querySelector('.btn-letter');
+    const answerText = answerTemplate.querySelector('.question-answer')
+
+    letterBtn.textContent = letters[i]
+    answerText.textContent = answer['answer']
+
+    answerTemplate.setAttribute('correct-answer', answer['correct'])
+
+    answerTemplate.classList.remove('hide')
+    answerTemplate.classList.remove('answer-template')
+
+    answersBox.appendChild(answerTemplate)
+
+    answerTemplate.addEventListener('click', function(){
+      checkAnswer(this)
+    })
+
+  })
+
+  actualQuestion++;
+
+}
+
+function checkAnswer(btn) {
+
+  const buttons = answersBox.querySelectorAll('button')
+
+  buttons.forEach(function(button) {
+
+    if(button.getAttribute('correct-answer') === 'true') {
+
+      button.classList.add('correct-answer')
+
+      if(btn === button) {
+
+        points++
+
+      }
+
+    } else {
+
+      button.classList.add('wrong-answer')
+
+    }
+
+  })
+
+  nextQuestion()
+
+}
+
+function nextQuestion() {
+
+  setTimeout(function(){
+
+    if(actualQuestion >= questions.length) {
+
+      showSuccessMessage();
+      return;
+
+    }
+
+    createQuestion(actualQuestion)
+
+  }, 1500)
+
+}
+
+function showSuccessMessage() {
+
+  hideOrShow()
+
+  const score = ((points / questions.length) * 100).toFixed(2)
+
+  const displayScore = document.querySelector('#display-score span')
+
+  displayScore.textContent = score.toString();
+
+  const correctAnswer = document.querySelector('#correct-answers')
+
+  correctAnswer.textContent = points
+
+  const totalQuestion = document.querySelector('#questions-qty')
+  totalQuestion.textContent = questions.length
+
+}
+
+function hideOrShow() {
+
+  quizzContainer.classList.toggle('hide')
+  scoreContainer.classList.toggle('hide')
+
+}
+
+const restartBtn = document.querySelector('#restart')
+
+restartBtn.addEventListener('click', function(){
+
+  actualQuestion = 0
+  points = 0
+  hideOrShow()
+  init()
+  
+})
+
+init()
